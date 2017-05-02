c9bdadd819c91ea886c76a1ab32479ad25c7e8cd
kakahikari <kakahikari@gmail.com>
2017-05-02 17:11:25 +0800

    暫時移除game api, 使用本地資料

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const BUILD_DATE = '2017-4-26 15:42'
-export const API_URL = 'http://api.cqgame.me/'
+export const API_URL = 'http://api.cqgame.me'
 export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI1OGMxODZhMjViMTRlMjAwMDFhZTliMzQiLCJhY2NvdW50IjoidGVzdGNxOSIsIm93bmVyIjoiNThjMTg2YTI1YjE0ZTIwMDAxYWU5YjM0IiwicGFyZW50Ijoic2VsZiIsImN1cnJlbmN5IjoiQ05ZIiwiaWF0IjoxNDg5MDc3OTIyLCJpc3MiOiJDeXByZXNzIiwic3ViIjoiU1NUb2tlbiJ9.taOL28yYwQYrsMnHc_BVtOXsjr9WupHWOqVJ9fSjQNA'
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,20 +1,364 @@
-import xhr from './xhr/'
-import { ERROR_CODES } from './xhr/config'
+// import xhr from './xhr/'
+// import { ERROR_CODES } from './xhr/config'
 
 class GameService {
   getGameList = ({context}) => {
     return new Promise((resolve, reject) => {
-      return xhr({
-        url: 'gameboy/game/list/cq9',
-        method: 'get',
-        context
-      }).then((res) => {
-        return resolve(res)
-      })
-      .catch((err) => {
-        console.log(err)
-        return reject(context.$root.i18n(ERROR_CODES[err.toString()] || err))
-      })
+      // return xhr({
+      //   url: 'gameboy/game/list/cq9',
+      //   method: 'get',
+      //   context
+      // }).then((res) => {
+      //   return resolve(res)
+      // })
+      // .catch((err) => {
+      //   console.log(err)
+      //   return reject(context.$root.i18n(ERROR_CODES[err.toString()] || err))
+      // })
+      return resolve([
+        {
+          'gamecode': '1',
+          'gamehall': 'cq9',
+          'gamename': 'FruitKing',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '2',
+          'gamehall': 'cq9',
+          'gamename': 'GodOfChess',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '3',
+          'gamehall': 'cq9',
+          'gamename': 'VampireKiss',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '4',
+          'gamehall': 'cq9',
+          'gamename': 'WildTarzan',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '5',
+          'gamehall': 'cq9',
+          'gamename': 'Mr.Rich',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '6',
+          'gamehall': 'cq9',
+          'gamename': '1945',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '7',
+          'gamehall': 'cq9',
+          'gamename': 'RaveJump',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '8',
+          'gamehall': 'cq9',
+          'gamename': 'SoSweet',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '9',
+          'gamehall': 'cq9',
+          'gamename': 'ZhongKui',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '10',
+          'gamehall': 'cq9',
+          'gamename': 'LuckyBat',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '11',
+          'gamehall': 'cq9',
+          'gamename': 'Wonderland',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '12',
+          'gamehall': 'cq9',
+          'gamename': 'TreasureHouse',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '13',
+          'gamehall': 'cq9',
+          'gamename': 'SakuraLegend',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '14',
+          'gamehall': 'cq9',
+          'gamename': 'RichWitch',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '15',
+          'gamehall': 'cq9',
+          'gamename': 'GuGuGu',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '16',
+          'gamehall': 'cq9',
+          'gamename': 'Super5',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '17',
+          'gamehall': 'cq9',
+          'gamename': 'GreatLion',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '18',
+          'gamehall': 'cq9',
+          'gamename': 'MahjongKing',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '19',
+          'gamehall': 'cq9',
+          'gamename': 'HotSpin',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '20',
+          'gamehall': 'cq9',
+          'gamename': '888',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '21',
+          'gamehall': 'cq9',
+          'gamename': 'LegendOfTheWolf',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '22',
+          'gamehall': 'cq9',
+          'gamename': 'MonkeyOfficeLegend',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '23',
+          'gamehall': 'cq9',
+          'gamename': 'YuanBao',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '24',
+          'gamehall': 'cq9',
+          'gamename': 'RaveJump2',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '25',
+          'gamehall': 'cq9',
+          'gamename': 'PokerSLOT',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '26',
+          'gamehall': 'cq9',
+          'gamename': '777',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '27',
+          'gamehall': 'cq9',
+          'gamename': 'Magic World',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '28',
+          'gamehall': 'cq9',
+          'gamename': 'God of Cookery',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '29',
+          'gamehall': 'cq9',
+          'gamename': 'WaterWorld',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '30',
+          'gamehall': 'cq9',
+          'gamename': 'Warrior Legend',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }, {
+          'gamecode': '33',
+          'gamehall': 'cq9',
+          'gamename': 'Fire Chibi',
+          'gameplat': 'WEB,MOBILE',
+          'gametech': 'html5',
+          'gametype': 'slot',
+          'info': {
+            'lang': ['en', 'tw', 'cn']
+          },
+          'status': true
+        }
+      ])
     })
   }
 }
