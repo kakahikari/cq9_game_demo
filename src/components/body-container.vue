98c4a61fa3d23c38b2270effac1330a7e2ab79d0
kakahikari <kakahikari@gmail.com>
2017-05-06 17:32:26 +0800

    更新遊戲清單, 改變搜尋邏輯&標籤排列

--- src/components/body-container.vue
+++ src/components/body-container.vue
@@ -4,14 +4,15 @@
       logo.body-container__logo
       .body-container__search
         span.body-container__search__icon(@click="toggleSearch()" ":class"="{'js--actived': searchFocus}")
-        transition(name="input")
+        transition(name="input-fade")
           input(v-if="searchActive" v-model="search" @focus="setSearchFocus(true)" @blur="setSearchFocus(false)")
         span.body-container__search__close(v-if="searchActive" @click="toggleSearch()" ":class"="{'js--actived': searchFocus}")
-      .body-container__sort
-        span.all(@click="changeSort('all')" ":class"="{'js--actived': sort=='all'}") All
-        span(@click="changeSort('new')" ":class"="{'js--actived': sort=='new'}") {{ $root.i18n('New') }}
-        span(@click="changeSort('recommend')" ":class"="{'js--actived': sort=='recommend'}") {{ $root.i18n('Recommend') }}
-        span(@click="changeSort('hot')" ":class"="{'js--actived': sort=='hot'}") {{ $root.i18n('Hot') }}
+      transition(name="sort")
+        .body-container__sort(v-if="!searchActive")
+          span(@click="changeSort('hot')" ":class"="{'js--actived': sort=='hot'}") {{ $root.i18n('Hot') }}
+          span(@click="changeSort('recommend')" ":class"="{'js--actived': sort=='recommend'}") {{ $root.i18n('Recommend') }}
+          span(@click="changeSort('new')" ":class"="{'js--actived': sort=='new'}") {{ $root.i18n('New') }}
+          span.all(@click="changeSort('all')" ":class"="{'js--actived': sort=='all'}") All
     v-bar(wrapper="body-container__wrapper" ref="vBar")
       .body-container__body
         .body-container__body__top-btn(@click="goTop()")
@@ -55,8 +56,9 @@
       return {
         sort: 'all',
         list: [],
-        hot: ['1', '8', '17', '19'],
-        recommend: ['7', '10', '14', '20'],
+        new: ['31', '35', '36', '40', '44', '45'],
+        hot: ['8', '9', '15', '12', '24', '1', '10', '7'],
+        recommend: ['37', '27', '39', '33'],
         searchActive: false,
         searchFocus: false,
         search: ''
@@ -80,16 +82,17 @@
             if (this.hot.indexOf(node.gamecode) > -1) {
               row.hot = 1
             }
-            if (array.length - index < 7) {
+            if (this.new.indexOf(node.gamecode) > -1) {
               row.new = 1
             }
             out.push(row)
           })
-          if (this.sort !== 'all') {
-            out = out.filter(node => node[this.sort] === 1)
-          }
           if (this.search !== '') {
             out = out.filter(node => this.$root.i18n(node.gamename).toLowerCase().indexOf(this.search.toLowerCase()) > -1)
+          } else {
+            if (this.sort !== 'all') {
+              out = out.filter(node => node[this.sort] === 1)
+            }
           }
           return out.reverse()
         } else {
@@ -122,14 +125,16 @@
       goTop () {
         let start = this.$refs.vBar.$el.getElementsByClassName('bar--wrapper')[0].scrollTop
         let intervel = setInterval(() => {
-          if (start > 1000) {
-            start -= 50
-          } else if (start > 500) {
-            start -= 30
-          } else if (start > 0) {
-            start -= 20
-          } else {
+          if (start < 0) {
             clearInterval(intervel)
+          } else if (start < 100) {
+            start -= 10
+          } else if (start < 500) {
+            start -= 40
+          } else if (start < 1000) {
+            start -= 100
+          } else {
+            start -= 200
           }
           this.$refs.vBar.$el.getElementsByClassName('bar--wrapper')[0].scrollTop = start
         }, 10)
